e1956edd793991a94ad2d31572b94346eea977f4
ahmdafif30 <ahmadafif5092@gmail.com>
2024-07-26 15:10:28 +0700

    revisi

--- Tabs/visualise.py
+++ Tabs/visualise.py
@@ -9,7 +9,6 @@ from web_function import train_model
 
 def app(df, x, y):
     warnings.filterwarnings('ignore')
-    st.set_option('deprecation.showPyplotGlobalUse', False)
 
     st.title("Visualisasi Prediksi Penyakit Batu Ginjal")
 
@@ -26,3 +25,6 @@ def app(df, x, y):
             filled=True, rounded=True, feature_names=x.columns, class_names=['0', '1']
         )
         st.graphviz_chart(dot_data)
+
+# Memanggil fungsi app dengan DataFrame yang sesuai
+# app(df, x, y)
